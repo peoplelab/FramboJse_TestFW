5827dcfc18bf9c4422322b4ac2c70bf885564565
an.bellini <an.bellini@reply.it>
2020-03-06 17:33:01 +0100

    add pure react page

---
+++ FramboJSe_Test/react-apps/DisplayCookie/DisplayCookieReact.jsx
@@ -0,0 +1,46 @@
+﻿
+// cookie handler
+const getDataFromCookie = () => {
+    const [framboCookie] = document.cookie.split(/;/);
+    const [key, value] = framboCookie.split(/=/);
+    const jsonVal = JSON.parse(value);
+    const data = jsonVal.result;
+
+    return { key, data };
+};
+
+// data list
+const cookiesList = cookie => cookie.split(/;/g).map((item) => {
+    const [key, value] = item.split(/=/);
+
+    return (
+        <li key={key}>
+            <b>{key}</b> {value}
+        </li>
+    );
+});
+
+// main component
+const App = (props) => {
+    const obj = React.useMemo(() => getDataFromCookie());
+    const List = React.useMemo(() => cookiesList(obj.data), [obj]);
+
+    return (
+        <div>
+            <h1>Test integrazione React con Yeap (pure React)</h1>
+            <br />
+            <div>{document.cookie}</div>
+            <br />
+            <p>{obj.key}</p>
+            <ul>
+                {List}
+            </ul>
+        </div>
+    );
+};
+
+
+
+// render react app
+const container = document.getElementById('root');
+ReactDOM.render(<App />, container);
---
+++ FramboJSe_Test/react-apps/common/template-import.js
@@ -0,0 +1,26 @@
+﻿//const getImport = document.querySelector('#react-head');
+//console.log(getImport);
+
+//// Clone the <template> in the import.
+//const template = getImport.import.querySelector('#head-template');
+//const clone = document.importNode(template.content, true);
+
+//document.head.appendChild(clone);
+
+
+const tags = 
+      '<script src="/system/3rd-parties/system/system.js"></script>'
+    + '<script src="/system/config.sys.js"></script>'
+    + '<script src="/system/3rd-parties/babel/babel.min.js"></script>'
+    //+ '<script src="/system/3rd-parties/react/react.development.js"></script>'
+    //+ '<script src="/system/3rd-parties/react/react-dom.development.js"></script>'
+
+    + '<link type="text/css" rel="stylesheet" href="/system/resources/css/overrides.sys.css" />'
+    + '<link type="text/css" rel="stylesheet" href="/system/resources/css/typographics.sys.css" />'
+    + '<link type="text/css" rel="stylesheet" href="/system/resources/css/structural.sys.css" />'
+    + '<link type="text/css" rel="stylesheet" href="/private/resources/css/overrides.pvt.css" />'
+    + '<link type="text/css" rel="stylesheet" href="/private/resources/css/typographics.pvt.css" />'
+    + '<link type="text/css" rel="stylesheet" href="/private/resources/css/application.pvt.css" />';
+
+
+document.head.innerHTML = document.head.innerHTML + tags;
